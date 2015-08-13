e6a28f9cc962080a1ca2b95ebdb0e36c305ee581
dahnny012 <filler012@gmail.com>
2015-08-13 04:41:26 -0500

    add tabbed sections format code

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,86 +1,92 @@
-(function () {
+(function() {
     var bitport = new Bitport();
-	var loginManager = new LoginManager();
+    var loginManager = new LoginManager();
 
     chrome.runtime.onMessage.addListener(
-        function (request, sender, sendResponse) {
+        function(request, sender, sendResponse) {
             switch (request.msg) {
-				case "loggedIn":
-					if (loginManager.login()) {
-						bitport.isLoggedIn().then(function (data) {
-							if (data) {
-								sendResponse({ loggedIn: true })
-							} else {
-								sendResponse({ loggedIn: false })
-							}
-						})
-					} else {
-						sendResponse({ loggedIn: true })
-					}
-					break;
+                case "loggedIn":
+                    if (loginManager.login()) {
+                        bitport.isLoggedIn().then(function(data) {
+                            if (data) {
+                                sendResponse({
+                                    loggedIn: true
+                                })
+                            } else {
+                                sendResponse({
+                                    loggedIn: false
+                                })
+                            }
+                        })
+                    } else {
+                        sendResponse({
+                            loggedIn: true
+                        })
+                    }
+                    break;
                 case "queue":
-					bitport.queueTorrents().then(function () {
-						sendResponse();
-					})
+                    bitport.queueTorrents().then(function() {
+                        sendResponse();
+                    })
                     break;
                 case "torrentStatus":
-					bitport.getTorrentStatus().then(function () {
-						sendResponse(bitport.transfers);
-					})
+                    bitport.getTorrentStatus().then(function() {
+                        sendResponse(bitport.transfers);
+                    })
                     break;
-				case "addedTorrents":
-					// User Recent added something
-					if (bitport.dirty) {
-						bitport.addPromise.then(function () {
-							bitport.getAddedTorrentStatus().then(function () {
-								sendResponse(bitport.addedTorrents);
-							})
-						})
-					}
-					// Hard scrape
-					else if (!bitport.addedTorrents) {
-						bitport.getAddedTorrents().then(function () {
-							sendResponse(bitport.addedTorrents);
-						})
-					} else {
-						// Not dirty
-						sendResponse(bitport.addedTorrents);
-					}
-					break;
-				case "addTorrent":
-					bitport.addTorrent(request.file);
-					break;
-				case "removeAdded":
-					bitport.removeAddedTorrent(request.index).then(function () {
-						sendResponse(bitport.addedTorrents);
-					})
-					break;
-				case "myFiles":
-					bitport.myFiles().then(function(data){
-						sendResponse(data);
-					});
-					break;
+                case "addedTorrents":
+                    // User Recent added something
+                    if (bitport.dirty) {
+                        bitport.addPromise.then(function() {
+                            bitport.getAddedTorrentStatus().then(function() {
+                                sendResponse(bitport.addedTorrents);
+                            })
+                        })
+                    }
+                    // Hard scrape
+                    else if (!bitport.addedTorrents) {
+                        bitport.getAddedTorrents().then(function() {
+                            sendResponse(bitport.addedTorrents);
+                        })
+                    } else {
+                        // Not dirty
+                        sendResponse(bitport.addedTorrents);
+                    }
+                    break;
+                case "addTorrent":
+                    bitport.addTorrent(request.file);
+                    break;
+                case "removeAdded":
+                    bitport.removeAddedTorrent(request.index).then(function() {
+                        sendResponse(bitport.addedTorrents);
+                    })
+                    break;
+                case "myFiles":
+                    bitport.myFiles().then(function(data) {
+                        sendResponse(data);
+                    });
+                    break;
                 default:
                     return true;
             }
-			return true;
+            return true;
         });
 })();
 
 function LoginManager() {
-	var ms = 1000;
-	var hour = 3600 * ms;
-	var minute = 60 * ms;
-	var checkInterval = 5 * minute;
-	var currentTime;
-	this.login = function () {
-		var checkTime = new Date();
-		if (currentTime) {
-			var difference = checkTime.valueOf() - currentTime;
-			return difference > checkInterval;
-		} else {
-			currentTime = checkTime.valueOf();
-			return true;
-		}
-	}
+    var ms = 1000;
+    var hour = 3600 * ms;
+    var minute = 60 * ms;
+    var checkInterval = 5 * minute;
+    var currentTime;
+    this.login = function() {
+        var checkTime = new Date();
+        if (currentTime) {
+            var difference = checkTime.valueOf() - currentTime;
+            return difference > checkInterval;
+        } else {
+            currentTime = checkTime.valueOf();
+            return true;
+        }
+    }
 }
--- gruntfile.js
+++ gruntfile.js
@@ -4,11 +4,21 @@ module.exports = function(grunt) {
             "default": {
                 src: ["app/scripts/*.js", "gruntfile.js"]
             },
+        },
+        'html-prettyprinter': {
+            single: {
+                // HTML file to beauty 
+                src: 'app/popup.html',
+
+                // Destination of HTML file 
+                dest: 'app/popup.html'
+            },
         }
     });
 
 
     grunt.loadNpmTasks("grunt-jsbeautifier");
-    grunt.registerTask('default', ['jsbeautifier']);
+    grunt.loadNpmTasks('grunt-html-prettyprinter');
+    grunt.registerTask('default', ['jsbeautifier', "html-prettyprinter"]);
 
 }
